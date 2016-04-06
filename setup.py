6f95b908eb20651afeb59693a3f74e7d7e6f5941
William Stein <wstein@sagemath.com>
2016-04-06 22:38:28 +0000

    initial version

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name        = 'sage_modabvar',
+      version     = '0.1',
+      description = 'Modular Abelian Varieties',
+      url         = '...todo...',
+      author      = 'William Stein, ...',
+      author_email= 'wstein@gmail.com',
+      license     = 'GPLv3',
+      packages    = ['sage_modabvar'],
+      zip_safe    = False)
